492c3a8f3b09a81962bbddaf5b7a6f0fcdb5c0fa
Musweta <kelvin.musweta@strathmore.edu>
2024-11-07 20:58:39 +0300

    Debuged the Profile file. Getline was not working correctly

--- Profile.cpp
+++ Profile.cpp
@@ -1,31 +1,31 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
 int main(){
-    //deeclare variable
+    //declare variable
     string firstName, lastName, fullName, favoriteMeal, favouriteMovie;
+   
     //prompt user for First Name
     cout<<"Please enter your first name";
     getline(cin,firstName);
-    cin>>firstName;
-    //Prompt user for Last Name
-     getline(cin, lastName);
-     cout<<"Please enter your last name";
-    cin>>lastName;
-     //Prompt user for full Name
-     getline(cin, fullName);
-     cout<<"Please enter your full name";
-    cin>>fullName;
 
+    //Prompt user for Last Name
+     cout<<"Please enter your last name";
+     getline(cin, lastName);
+
+     //Prompt user for full Name
+     // Combine first and last names into fullName
+     cout<<"Please enter your full name";
+     getline(cin, fullName);
+    
      //Prompt user for favourite meal
-     getline(cin, favoriteMeal);
      cout<<"Please enter your favourite meal";
-    cin>>favoriteMeal;
+     getline(cin, favoriteMeal);
     
      //Prompt user for favourite movie
+    cout<<"Please enter your favourite movie";
      getline(cin, favouriteMovie);
-     cout<<"Please enter your favourite movie";
-    cin>>favouriteMovie;
 
     cout<<"My first name is "<<firstName<<endl;
     cout<<"My Last name is "<<lastName<<endl;
